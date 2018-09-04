a8b9e1c0741009dbced8cc7e211bbc4f4555adb1
Jam Risser <jam@codejam.ninja>
2018-09-03 20:56:56 -0500

    Initial commit

---
+++ src/base.js
@@ -0,0 +1,29 @@
+export default {
+  extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  rules: {
+    'arrow-body-style': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-return-assign': 'off',
+    'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        vars: 'all'
+      }
+    ],
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['^~', '^./lib']
+      }
+    ],
+    'comma-dangle': ['error', 'never']
+  }
+};
