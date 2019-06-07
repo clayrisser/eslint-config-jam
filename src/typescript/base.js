7254ea1a05db2b8724161e8930c6bfb868e08c9f
Jam Risser <jam@codejam.ninja>
2019-06-06 21:00:04 -0500

    Updated typescript

--- src/typescript/base.js
+++ src/typescript/base.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['airbnb-typescript/base', 'prettier', '../lib/rules/base.js'],
   parser: '@typescript-eslint/parser',
   rules: {
+    '@typescript-eslint/indent': 'off',
     'import/prefer-default-export': 'off',
     'no-empty-function': ['error', { allow: ['constructors'] }],
     'no-useless-constructor': 'off'
