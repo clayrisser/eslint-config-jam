791552a88c1e5573125ad6d7071cdef7ebe31953
Jam Risser <jam@codejam.ninja>
2019-08-30 02:31:54 -0500

    v0.2.18

--- src/typescript/index.js
+++ src/typescript/index.js
@@ -1,10 +1,3 @@
-import { resolvePath } from '../util';
-
 module.exports = {
-  extends: [
-    'airbnb-typescript',
-    resolvePath('./lib/typescript/base', './base'),
-    resolvePath('./lib/rules/jest', '../rules/jest'),
-    resolvePath('./lib/rules/react', '../rules/react')
-  ]
+  extends: ['airbnb-typescript', './base', '../rules/jest', '../rules/react']
 };
