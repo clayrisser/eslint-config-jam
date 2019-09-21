a208b29d5c03680e883d7cc8b88590bd750adad1
Jam Risser <jam@codejam.ninja>
2019-09-21 23:33:09 +0000

    Updated resolvePath function

--- src/util.js
+++ src/util.js
@@ -1,8 +1,13 @@
 import fs from 'fs-extra';
 
 export function resolvePath(name, nextName) {
-  const path = require.resolve(name);
-  if (!fs.existsSync(path)) return resolvePath(nextName);
+  let path = name;
+  try {
+    path = require.resolve(name);
+  } catch (err) {
+    if (nextName) path = resolvePath(nextName);
+  }
+  if (!fs.existsSync(path) && nextName) return resolvePath(nextName);
   return path;
 }
 
